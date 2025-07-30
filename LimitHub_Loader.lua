1c34ee76625246c2c09a23776cf0e1cb03cf3e29
LimitHub <120258153+FakeModzL@users.noreply.github.com>
2025-07-30 11:53:16 +0800

    LimitHub_Loader.lua

---
+++ LimitHub_Loader.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://pastefy.app/IZm17Kpn/raw"))()
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/FakeModz/LimitHub/refs/heads/main/LimitHub_Loader.lua')))()
